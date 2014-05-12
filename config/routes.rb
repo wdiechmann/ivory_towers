b80632af11ccd402b92d166da763599ad6aae515
Enrique Phillips <enrique.phillips.wac@gmail.com>
2014-05-12 16:55:25 +0200

    named app and set gitignore
    named the App in config/routes.rb and made sure .gitignore does not
    look for .ruby-version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails3DeviseRspecCucumber::Application.routes.draw do
+IvoryTowers::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
