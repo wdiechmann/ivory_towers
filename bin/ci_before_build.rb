d85e5915ce063c7ca971eda29cbde870d4b28d66
Enrique Phillips <enrique.phillips.wac@gmail.com>
2014-05-12 10:26:53 +0200

    write config/database.yml for CI

--- bin/ci_before_build.rb
+++ bin/ci_before_build.rb
@@ -1,14 +1,15 @@
 unless File.exist? "config/database.yml"
-  f = File.new "config/database.yml"
-  f.put "
-    test:
-      adapter: mysql2
-      encoding: utf8
-      reconnect: false
-      database: ivory_towers_test
-      pool: 5
-      username: shippable
-      password: 
-      host: localhost
-  "
+  File.open( "config/database.yml", "w+") do |f|
+    f.puts "
+test:
+  adapter: mysql2
+  encoding: utf8
+  reconnect: false
+  database: ivory_towers_test
+  pool: 5
+  username: shippable
+  password: 
+  host: localhost
+"
+  end
 end
